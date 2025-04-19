dd4289ded92735abffffce4857ace7b1d87fe9f8
jeff <johnjeff55@gmail.com>
2025-04-19 11:40:36 -0700

    Update scripts/validate-manifest.js

--- scripts/validate-manifest.js
+++ scripts/validate-manifest.js
@@ -4,9 +4,9 @@ const manifest = require('../deploy/manifest.json')
 if (!manifest.key) {
   console.error('Missing manifest key')
   process.exit(1)
-} else console.log(`Validate Manifest: ${manifest.key}`)
+}
 
 if (manifest.manifest_version !== 3) {
   console.error('Invalid manifest version')
   process.exit(1)
-} else console.log(`Validate Manifest: ${manifest.manifest_version}`)
+}
